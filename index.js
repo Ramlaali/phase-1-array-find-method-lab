7f725b9fd8cbd56f11a142d1fdfce63d48aa5b1a
Ramla Abdullahi <ramlaali.abd@gmail.com>
2021-06-17 23:30:25 -0400

    commit tree 155d043ea784173f0ac05fe18ccd088ec4c82a2d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function superbowlWin(record){
+   
+   const found = record.find(record => record.result === `W`);
+   console.log(found)
+
+   if (found) {
+       return found.year;
+   } else {
+       return undefined
+   }
+
+    
+
+    
+    
+
+}
+
+
+
 
